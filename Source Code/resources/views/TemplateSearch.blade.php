77f4feae79d4985a34e21386fe82155f178a45b5
DalvesR <u14347980@tuks.co.za>
2018-05-09 14:35:26 +0200

    Adding interface.

--- Source Code/resources/views/TemplateSearch.blade.php
+++ Source Code/resources/views/TemplateSearch.blade.php
@@ -1,15 +1,4 @@
 <!DOCTYPE html>
-<!-- ==============================
-    Project:        Metronic "Asentus" Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 3.3.4
-    Version:        1.0
-    Author:         KeenThemes
-    Primary use:    Corporate, Business Themes.
-    Email:          support@keenthemes.com
-    Follow:         http://www.twitter.com/keenthemes
-    Like:           http://www.facebook.com/keenthemes
-    Website:        http://www.keenthemes.com
-    Premium:        Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-================================== -->
 <html lang="en" class="no-js">
     <!-- BEGIN HEAD -->
     <head>
@@ -20,67 +9,15 @@
         <meta content="" name="description"/>
         <meta content="" name="author"/>
 
-        <!-- GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet" type="text/css">
-        <link href="vendor/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-
-        <!-- PAGE LEVEL PLUGIN STYLES -->
-        <link href="css/animate.css" rel="stylesheet">
-
-        <!-- THEME STYLES -->
-        <link href="css/layout.min.css" rel="stylesheet" type="text/css"/>
-
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="favicon.ico"/>
+        @extends('layout.app')
     </head>
     <!-- END HEAD -->
 
     <!-- BODY -->
     <body>
 
-         <!--========== HEADER ==========-->
-        <header class="header navbar-fixed-top">
-            <!-- Navbar -->
-            <nav class="navbar" role="navigation">
-                <div class="container">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="menu-container">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="toggle-icon"></span>
-                        </button>
-
-                        <!-- Logo -->
-                        <div class="logo">
-                            <a class="logo-wrap" href="index.html">
-                                <img  width="300" src="img/ThutongLogo.png" alt="ThutongLogoSize">
-                            </a>
-                        </div>
-                        <!-- End Logo -->
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse nav-collapse">
-                        <div class="menu-container">
-                            <ul class="navbar-nav navbar-nav-right">
-                                
-                                <li class="nav-item"><a style="font-size: 20px" class="nav-item-child nav-item-hover active" href="./homeP">Home</a></li>
-                                <li class="nav-item"><a style="font-size: 12px" class="nav-item-child nav-item-hover" href="./about">About</a></li>
-                                <li class="nav-item"><a style="font-size: 12px" class="nav-item-child nav-item-hover" href="./subjectList">Subjects</a></li>
-                                <li class="nav-item"><a style="font-size: 12px" class="nav-item-child nav-item-hover" href="./showAllJobs">Jobs</a></li>
-                                <li class="nav-item"><a style="font-size: 12px" class="nav-item-child nav-item-hover" href="./contact">Contact Us</a></li>
-                                <li class="nav-item"><a style="font-size: 20px" class="nav-item-child nav-item-hover" href="./profile">
-                                    Admin: Dan Can</a></li>
-                                <li class="nav-item"><a style="font-size: 10px" class="nav-item-child nav-item-hover" href="./"> Login screen </a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- End Navbar Collapse -->
-                </div>
-            </nav>
-            <!-- Navbar -->
-        </header>
+        <!--========== HEADER ==========-->
+        @extends('layout.Navbar')
         <!--========== END HEADER ==========-->
 
         <!--========== PARALLAX ==========-->
@@ -230,27 +167,16 @@
 
 
         <!-- Back To Top -->
-        <a href="javascript:void(0);" class="js-back-to-top back-to-top">Top</a>
-
-        <!-- JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-        <!-- CORE PLUGINS -->
-        <script src="vendor/jquery.min.js" type="text/javascript"></script>
-        <script src="vendor/jquery-migrate.min.js" type="text/javascript"></script>
-        <script src="vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-
-        <!-- PAGE LEVEL PLUGINS -->
-        <script src="vendor/jquery.easing.js" type="text/javascript"></script>
-        <script src="vendor/jquery.back-to-top.js" type="text/javascript"></script>
-        <script src="vendor/jquery.smooth-scroll.js" type="text/javascript"></script>
-        <script src="vendor/jquery.wow.min.js" type="text/javascript"></script>
-        <script src="vendor/jquery.parallax.min.js" type="text/javascript"></script>
-
-        <!-- PAGE LEVEL SCRIPTS -->
-        <script src="js/layout.min.js" type="text/javascript"></script>
-        <script src="js/components/wow.min.js" type="text/javascript"></script>
-        <script src="js/components/gmap.min.js" type="text/javascript"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsXUGTFS09pLVdsYEE9YrO2y4IAncAO2U&amp;callback=initMap" async defer></script>
-
+        @extends('layout.endLinks')
     </body>
     <!-- END BODY -->
 </html>
+
+
+
+ <!-- External Links Ignore ///////////////////////////////////////////////////////////////////// -->
+ <!-- External Links Ignore ///////////////////////////////////////////////////////////////////// -->
+ <!-- External Links Ignore ///////////////////////////////////////////////////////////////////// -->
+ <!-- External Links Ignore ///////////////////////////////////////////////////////////////////// -->
+
+ 
