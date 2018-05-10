5f4d6dbace0d6eca8854cc2ea511d780470910a6
DalvesR <u14347980@tuks.co.za>
2018-05-10 20:32:24 +0200

    Added quiz and subjects list.

--- Source Code/resources/views/SubjectList.blade.php
+++ Source Code/resources/views/SubjectList.blade.php
@@ -15,28 +15,137 @@
                     <div class="row" style="padding-left:100px;">
 
                 <!-- PHP output list format -->
-                    <h1> -> Subject <h1>
-                    <h2> ---------> Grade <h2>
-                    <h2> ---------> Topic <h2>
-                    <a href="./lesson" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Lesson </a>
-                    <a href="./quiz" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Quiz </a>
-                   
-                 
-                        </br>
-                        <h1> -> Subject <h1>
-                    <h2> ---------> Grade <h2>
-                    <h2> ---------> Topic <h2>
-                    <h2> --------------> Lesson <h2>
-                    <h2> --------------> Quiz <h2>
-                    <a href="./jobs" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Lesson </a>
-                    <a href="./jobs" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Lesson </a>
-                 
-                    <!--// end row -->
+                            <details>
+                                <summary> <a class="btn-theme btn-theme-sm " style="font-size:40px"> > Grade 8 </a>
+                                </summary>
+                                    <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (Science) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Molecules) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Molecules) </a> </br> </br>
+                                        </details>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Chemsitry) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Chemistry) </a> </br> </br>
+                                        </details>   
+                                </details>
+                                <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (Biology) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Brain) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Brain) </a> </br> </br>
+                                        </details>   
+                                </details>   
+                            </details>   
+
+                            </br>
+                            </br>
+
+                            <details>
+                                <summary> <a class="btn-theme btn-theme-sm " style="font-size:40px"> > Grade 10 </a>
+                                </summary>
+                                    <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (Science) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Molecules) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Molecules) </a> </br> </br>
+                                        </details>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Chemsitry) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Chemistry) </a> </br> </br>
+                                        </details>   
+                                </details>
+                                <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (Geography) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Maps) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (maps) </a> </br> </br>
+                                        </details>   
+                                </details>   
+                            </details>   
+
+                            </br>
+                            </br>
+                       
+                            <details>
+                                <summary> <a class="btn-theme btn-theme-sm " style="font-size:40px; color:red'"> > Grade 12 </a>
+                                </summary>
+                                    <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (Maths) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Binomials) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Binomials) </a> </br> </br>
+                                        </details>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (Trig) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (Trig) </a> </br> </br>
+                                        </details>   
+                                </details>
+                                <details>
+                                    <summary class="btn-theme btn-theme-sm " style="padding-left: 50px;"> <a style="font-size:30px">  Subject (History) </a>
+                                    </summary>
+                                        <details>
+                                        <summary style="padding-left: 50px;"> <a style="font-size:20px">  Topic (WW2) </a>
+                                        </summary>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 1 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 2 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 3 </a> </br>
+                                            <a href="/lesson" style="padding-left: 250px;"> - Lesson 4 </a> </br>
+                                            <a href="./quiz" style="padding-left: 250px;" class="btn-theme btn-theme-sm "> Quiz (WW2) </a> </br> </br>
+                                        </details>   
+                                </details>   
+                            </details>   
+
+
                 </div>
-                
             </div>
-            <a href="./jobsAdd" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Jobs Add</a>
-            <a href="./jobsManage" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Jobs Manage</a>
+            <a href="./jobsAdd" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Subjects Add</a>
+            <a href="./jobsManage" class="btn-theme btn-theme-sm btn-base-bg text-uppercase"> Subjects Manage</a>
         </footer>
             <!-- End Links -->
       @endsection
