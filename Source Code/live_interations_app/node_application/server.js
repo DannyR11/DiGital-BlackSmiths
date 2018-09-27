6b0e35744e5392e6596d142cf92955a0b8cefc82
LesegoGomolemo <lesegomabe@ymail.com>
2018-09-27 16:25:01 +0200

    Show public directory

--- Source Code/live_interations_app/node_application/server.js
+++ Source Code/live_interations_app/node_application/server.js
@@ -23,6 +23,7 @@ app.use(express.static('public'));
 // Provide access to node_modules folder from the client-side
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 app.use('/client', express.static(`${__dirname}/public/client.html`));
+app.use(express.static(__dirname + '/public'));
 // Redirect all traffic to index.html
 //app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
