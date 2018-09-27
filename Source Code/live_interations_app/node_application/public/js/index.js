fed6169cdc713bc760c9af24d047596be110c095
LesegoGomolemo <lesegomabe@ymail.com>
2018-09-27 14:39:49 +0200

    index.js line 113 fixed to xhttp

--- Source Code/live_interations_app/node_application/public/js/index.js
+++ Source Code/live_interations_app/node_application/public/js/index.js
@@ -110,7 +110,7 @@ function loadName() {
 	xhttp.onreadystatechange = 
 		function() {
 			if (this.readyState == 4 && this.status == 200) {
-				teacherName = x.responseText;
+				teacherName = xhttp.responseText;
 			}
 		};
 	
