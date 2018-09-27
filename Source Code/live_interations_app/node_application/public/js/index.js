00aa0f432cfc9753647f7ba17bbbad83225c3da4
LesegoGomolemo <lesegomabe@ymail.com>
2018-09-27 15:46:14 +0200

    Ajax Debugging

--- Source Code/live_interations_app/node_application/public/js/index.js
+++ Source Code/live_interations_app/node_application/public/js/index.js
@@ -125,7 +125,7 @@ function loadName() {
 			}
 		};
 	
-	xhttp.open("GET", "http://137.215.42.239/moodle/local/testplugin/client/client.php", true);
+	xhttp.open("GET", "https://137.215.42.239/moodle/local/testplugin/client/client.txt", true);
 	xhttp.send();
 }
   
